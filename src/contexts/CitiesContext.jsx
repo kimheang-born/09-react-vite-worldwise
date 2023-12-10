a3f2aaca5bf4c232828bf46e08718e6c1b679571
Kimheang <kimheang135@gmail.com>
2023-12-10 17:18:15 +0700

    feat: finish the city view

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -8,6 +8,7 @@ const CitiesContext = createContext();
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
 
   useEffect(() => {
     const fetchCities = async () => {
@@ -20,6 +21,7 @@ function CitiesProvider({ children }) {
         setCities(data);
       } catch (err) {
         console.log(err);
+        alert('There was an error loading data...');
       } finally {
         setIsLoading(false);
       }
@@ -28,11 +30,29 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
+  const getCity = async (id) => {
+    try {
+      setIsLoading(true);
+
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+
+      setCurrentCity(data);
+    } catch (err) {
+      console.log(err);
+      alert('There was an error loading data...');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <CitiesContext.Provider
       value={{
         cities,
         isLoading,
+        currentCity,
+        getCity,
       }}
     >
       {children}
